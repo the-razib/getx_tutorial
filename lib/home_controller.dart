0af5b43b729eae005a4da0f25e64338158670a37
Md Razib <mdrazib.cse@gmail.com>
2025-04-30 08:57:50 +0600

    Color opacity change with getx

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -1,5 +1,8 @@
 import 'package:get/get.dart';
 
 class HomeController extends GetxController{
-
+RxDouble  opacity=0.0.obs;
+ changeValue(value){
+   opacity.value=value;
+ }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_tutorial/home_controller.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,25 +10,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double opacity=0.0;
   @override
   Widget build(BuildContext context) {
+    final HomeController controller = Get.put(HomeController());
     print('build');
-    
     return Scaffold(
       body: Center(
         child: Column(
           children: [
-            Container(
-              height: Get.height*0.3,
-              color: Colors.greenAccent.withValues(alpha: opacity),
-            ),
-            Slider(value: opacity, onChanged: (value){
-              setState(() {
-                opacity =value;
-                print(value);
+            Obx(() {
+              return Container(
+                height: Get.height * 0.3,
+                color: Colors.greenAccent.withValues(
+                    alpha: controller.opacity.value),
+              );
+            }),
+
+            Obx(() {
+              return Slider(
+                  value: controller.opacity.value, onChanged: (value) {
+                    controller.changeValue(value);
               });
             })
+
           ],
         ),
       ),
